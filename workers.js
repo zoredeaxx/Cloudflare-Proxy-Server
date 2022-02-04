6400a8888988bad164f936019c78f45b4c9077d6
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-02-04 22:21:30 +0530

    Update workers.js

--- workers.js
+++ workers.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://redop1189.github.io/proxyapi'
+const ASSET_URL = 'https://github.com/zoredeaxx/proxyapi'
 
 const JS_VER = 10
 const MAX_RETRY = 1
